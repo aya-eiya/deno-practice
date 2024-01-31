df0ebd1f0fcdd9fa05cca65fe0eb141956055acc
h.ayabe <h.ayabe_biz@live.jp>
2024-01-31 18:22:56 +0900

    deploy test

--- user-web/routes/index.tsx
+++ user-web/routes/index.tsx
@@ -16,8 +16,7 @@ export default function Home() {
         />
         <h1 class='text-4xl font-bold'>Welcome to Fresh</h1>
         <p class='my-4'>
-          Try updating this message in the
-          <code class='mx-2'>./routes/index.tsx</code> file, and refresh.
+          Fresh is a framework for building web apps with Preact and Deno.
         </p>
         <Counter count={count} isOpen={isOpen} />
       </div>
