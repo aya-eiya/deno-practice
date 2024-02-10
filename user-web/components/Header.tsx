67aaf3ada713b846375bde3b3fe392db28269ede
h.ayabe <h.ayabe_biz@live.jp>
2024-02-10 17:28:40 +0900

    wip

--- user-web/components/Header.tsx
+++ user-web/components/Header.tsx
@@ -1,35 +1,49 @@
 import DrawerMenu from '../islands/menu/DrawerMenu.tsx'
 
 export default function Header() {
+  const mainTitle = {
+    title: 'Title Logo',
+    subtitle: 'Denoの練習サイト',
+    desc: 'Denoの練習サイトです\nDenoはV8エンジンとTypeScriptを使っています。',
+    image: '/logo.svg',
+  }
+  const contactUs = {
+    buttonTitle: 'Contact Us',
+  }
   const menuItems = {
     'Home': {
       path: '/',
       title: 'Home',
+      desc: 'ホーム',
     },
     'About': {
       path: '/about',
       title: 'About',
+      desc: '私たちについて・会社概要',
     },
     'News': {
       path: '/news',
       title: 'News',
+      desc: 'ニュース・お知らせ',
     },
     'Projects': {
       path: '/projects',
       title: 'Projects',
+      desc: '導入事例・プロジェクト',
     },
     'Contact': {
       path: '/contact',
       title: 'Contact',
+      desc: 'お問い合わせ・お見積もり',
     },
   }
   return (
-    <header className={'sticky w-full h-24 flex flex-row justify-between'}>
-      <div className={'header-logo w-1/2 flex items-center pl-8'}>
+    <header className={'sticky w-full h-32 flex flex-row justify-between items-end px-12'}>
+      <div className={'header-logo w-1/2 flex items-center pl-8 py-6'}>
         <p className={'flex px-2 text-lg'}>Title Logo</p>
       </div>
-      <nav className={'w-5/6 flex content-center justify-center'}>
-        <div className={'header-menu w-1/2 flex flex-row justify-between items-center'}>
+      <nav className={'w-5/6 flex content-center justify-end mr-10'}>
+        <div className={'header-menu w-1/2 flex flex-row justify-between items-center py-6'}>
           <div className={'menu-item px-2'}>{menuItems.Home.title}</div>
           <div className={'menu-item px-2'}>{menuItems.About.title}</div>
           <div className={'menu-item px-2'}>{menuItems.News.title}</div>
@@ -40,12 +54,37 @@ export default function Header() {
       <nav>
         <div className={'header-drawer-menu'}>
           <DrawerMenu>
-            <div className={'bg-white w-screen'}>
-              <div className={'menu-item'}>{menuItems.Home.title}</div>
-              <div className={'menu-item'}>{menuItems.About.title}</div>
-              <div className={'menu-item'}>{menuItems.News.title}</div>
-              <div className={'menu-item'}>{menuItems.Projects.title}</div>
-              <div className={'menu-item'}>{menuItems.Contact.title}</div>
+            <div className={'bg-gray-100 w-screen h-screen flex flex-row justify-center items-center'}>
+              <div className={'w-1/5 flex flex-col'}>
+                <div className={'menu-item pb-5'}>
+                  <p className={'text-6xl font-bold'}>{menuItems.Home.title}</p>
+                  <p className={'text-gray-500'}>{menuItems.Home.desc}</p>
+                </div>
+                <div className={'menu-item pb-5'}>
+                  <p className={'text-6xl font-bold'}>{menuItems.About.title}</p>
+                  <p className={'text-gray-500'}>{menuItems.About.desc}</p>
+                </div>
+                <div className={'menu-item pb-5'}>
+                  <p className={'text-6xl font-bold'}>{menuItems.News.title}</p>
+                  <p className={'text-gray-500'}>{menuItems.News.desc}</p>
+                </div>
+                <div className={'menu-item pb-5'}>
+                  <p className={'text-6xl font-bold'}>{menuItems.Projects.title}</p>
+                  <p className={'text-gray-500'}>{menuItems.Projects.desc}</p>
+                </div>
+                <div className={'menu-item pb-5'}>
+                  <p className={'text-6xl font-bold'}>{menuItems.Contact.title}</p>
+                  <p className={'text-gray-500'}>{menuItems.Contact.desc}</p>
+                </div>
+              </div>
+              <div className={'w-2/5 flex flex-col items-center'}>
+                <p className={'text-8xl font-bold mb-3'}>{mainTitle.title}</p>
+                <p className={'mb-3'}>{mainTitle.subtitle}</p>
+                <div className={'flex w-full flex-row justify-center items-center'}>
+                  <p className={'whitespace-pre text-sm mr-5'}>{mainTitle.desc}</p>
+                  <button className={'rounded-full bg-yellow-400 px-6 py-4'}>{contactUs.buttonTitle}</button>
+                </div>
+              </div>
             </div>
           </DrawerMenu>
         </div>
--- user-web/routes/index.tsx
+++ user-web/routes/index.tsx
@@ -4,21 +4,35 @@ import Counter from '../islands/dialog/Counter.tsx'
 export default function Home() {
   const count = useSignal(3)
   const isOpen = useSignal(false)
+
+  const mainTitle = {
+    title: 'Title Logo',
+    subtitle: 'Denoの練習サイト',
+    fullDesc: 'Denoの練習サイトです\nDenoはV8エンジンとTypeScriptを使っています\nFreshはDenoのフレームワークです',
+    image: '/logo.svg',
+  }
   return (
-    <div class='px-4 py-8 mx-auto bg-[#86efac]'>
-      <div class='max-w-screen-md mx-auto flex flex-col items-center justify-center'>
-        <img
-          class='my-6'
-          src='/logo.svg'
-          width='128'
-          height='128'
-          alt='the Fresh logo: a sliced lemon dripping with juice'
-        />
-        <h1 class='text-4xl font-bold'>Welcome to Fresh</h1>
-        <p class='my-4'>
-          Fresh is a framework for building web apps with Preact and Deno.
-        </p>
-        <Counter count={count} isOpen={isOpen} />
+    <div className={'w-full flex'}>
+      <div className={'h-screen w-full flex flex-row'}>
+        <div className={'w-1/2'}>
+          <img
+            className={'my-6'}
+            src={mainTitle.image}
+            width='128'
+            height='128'
+            alt='the Fresh logo: a sliced lemon dripping with juice'
+          />
+        </div>
+        <div className={'w-1/2 flex flex-col items-center justify-center pb-24 text-red-500'}>
+          <p className={'text-xl font-bold'}>{mainTitle.subtitle}</p>
+          <h1 className={'text-8xl font-bold my-3'}>{mainTitle.title}</h1>
+          <div className={'whitespace-pre my-5'}>
+            {mainTitle.fullDesc}
+          </div>
+          <div className={'flex'}>
+            <Counter count={count} isOpen={isOpen} />
+          </div>
+        </div>
       </div>
     </div>
   )
