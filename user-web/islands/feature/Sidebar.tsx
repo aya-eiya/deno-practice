48998820d4136175466030de598441ae803e84a3
h.ayabe <h.ayabe_biz@live.jp>
2024-03-01 13:55:03 +0900

    wip

--- user-web/islands/feature/Sidebar.tsx
+++ user-web/islands/feature/Sidebar.tsx
@@ -1,20 +1,65 @@
 import { useSignal } from '@preact/signals'
 import Feedback from '../scrollY/Feedback.tsx'
+import { useEffect, useState } from 'preact/hooks'
 
 export default function Sidebar({ ids }: { ids: string[] }) {
-  const top = useSignal(undefined)
-  const bottom = useSignal(undefined)
+  const top = useSignal(0)
+  const bottom = useSignal(0)
+  const clientHeight = useSignal(0)
+  const [colors, setColors] = useState([
+    'text-red-500',
+    'text-blue-500',
+    'text-green-500',
+    'text-yellow-500',
+    'text-pink-500',
+  ])
+  const altColors = [
+    'text-gray-100',
+    'text-gray-100',
+    'text-gray-100',
+    'text-gray-100',
+    'text-gray-100',
+  ]
+  const animeColors = [
+    'text-red-500    animate-color-fade-in',
+    'text-blue-500   animate-color-fade-in',
+    'text-green-500  animate-color-fade-in',
+    'text-yellow-500 animate-color-fade-in',
+    'text-pink-500   animate-color-fade-in',
+  ]
+  const animeAltColors = [
+    'text-red-500    animate-color-fade-out saturate-0',
+    'text-blue-500   animate-color-fade-out saturate-0',
+    'text-green-500  animate-color-fade-out saturate-0',
+    'text-yellow-500 animate-color-fade-out saturate-0',
+    'text-pink-500   animate-color-fade-out saturate-0',
+  ]
+  useEffect(() => {
+    clientHeight.value = globalThis.innerHeight
+  }, [])
+  const isVisible = top.value && bottom.value && (top.value > -180 && bottom.value < 320)
+
   return (
     <>
       {ids.length > 0 && <Feedback scrollY={top} fromById={ids[0]} />}
       {ids.length > 1 && <Feedback scrollY={bottom} fromById={ids[ids.length - 1]} />}
       <div
         className={'fixed top-0 h-screen hidden ' +
-          (top.value && bottom.value && (top.value > -180 && bottom.value < 320) ? 'md:flex' : '') +
+          (isVisible ? 'md:flex animate-slide-in-left' : 'md:flex animate-slide-out-left translate-x-[-300%]') +
           ' md:w-11/12 lg:w-10/12 flex-col justify-center items-start'}
       >
-        {ids.map((id, idx) => <div className={'text-7xl font-bold my-2'}>{String(idx + 1).padStart(2, '0')}</div>)}
-        {top.value} - {bottom.value} - {globalThis.screenY}
+        {ids.map((id, idx) => (
+          <div
+            key={id}
+            className={'text-7xl font-bold my-2 ' +
+              (top.value !== undefined &&
+                  (clientHeight.value * (idx - 0.5) <= top.value && top.value <= clientHeight.value * (idx + 0.5))
+                ? colors[idx % colors.length]
+                : altColors[idx % altColors.length])}
+          >
+            {String(idx + 1).padStart(2, '0')}
+          </div>
+        ))}
       </div>
     </>
   )
--- user-web/tailwind.config.ts
+++ user-web/tailwind.config.ts
@@ -36,6 +36,14 @@ export default {
           '0%': { transform: 'translateY(0%)' },
           '100%': { transform: 'translateY(-300%)' },
         },
+        'slide-in-left': {
+          '0%': { transform: 'translateX(-300%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+        'slide-out-left': {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-300%)' },
+        },
         'fade-in': {
           '0%': { opacity: '0' },
           '100%': { opacity: '1' },
@@ -43,6 +51,15 @@ export default {
         'fade-out': {
           '100%': { opacity: '0' },
           '0%': { opacity: '1' },
+        },
+        'color-fade-in': {
+          '0%': { filter: 'saturate(0%)' },
+          '100%': { filter: 'saturate(100%)' },
+        },
+        'color-fade-out': {
+          '0%': { filter: 'saturate(0%)' },
+          '50%': { filter: 'saturate(100%)' },
+          '100%': { color: 'gray-100' },
         },
         'drawer-effect': {
           '0%': { transform: 'translateY(64px)' },
@@ -64,8 +81,12 @@ export default {
         'v-infinity-scroll-reverse': 'v-infinity-scroll-reverse 6s ease-in-out infinite',
         'slide-down': 'slide-down .6s ease-in',
         'slide-up': 'slide-up 1s linear',
+        'slide-in-left': 'slide-in-left 1s ease-out',
+        'slide-out-left': 'slide-out-left 1s ease-out',
         'fade-in': 'fade-in .5s linear',
         'fade-out': 'fade-out .2s linear',
+        'color-fade-in': 'color-fade-in .5s linear',
+        'color-fade-out': 'color-fade-out .5s linear',
         'drawer-effect': 'drawer-effect .8s ease-out',
         'half-spin': 'half-spin .2s linear',
         'half-spin-reverse': 'half-spin-reverse .2s linear',
